471062d057c909851769515be7aab6da353fa6f1
kknmt <m20109@g.metro-cit.ac.jp>
2024-12-10 15:20:49 +0900

    Merge した

--- backend/app/router/customer.py
+++ backend/app/router/customer.py
@@ -5,10 +5,9 @@ from database import get_async_db
 from models import customer as models_customer
 from crud import customer
 
-from lib.auth import customer_login, get_current_customer
+from lib.auth import customer_login, customer_logout, get_current_customer
 
 router = APIRouter()
-
 
 # テンプレートのため要確認
 @router.post("/add_customer")
@@ -19,13 +18,24 @@ async def add_customer(add_customer_request: models_customer.AddCustomerRequest,
     points = 0
     return await customer.add_customer(db, name, email, password, points)
 
+@router.post("/edit_customer")
+async def edit_customer(edit_customer_request: models_customer.EditCustomerRequest, db = Depends(get_async_db), current_customer = Depends(get_current_customer)) -> models_customer.EditCustomerResponse:
+    id = current_customer.id
+    name = edit_customer_request.name
+    email = edit_customer_request.email
+    password = edit_customer_request.password
+    points = edit_customer_request.points
+    return await customer.edit_customer(id, db, name, email, password, points)
 
 @router.post("/login")
 async def login_customer(_ =  Depends(customer_login)):
     return
 
+@router.post("/logout")
+async def logout_customer(_ = Depends(customer_logout)):
+    return
 
 # ログイン済みのユーザのみアクセス可能
-# @router.post("/me")
-# async def me(current_customer = Depends(get_current_customer)):
-#     return current_customer
+@router.post("/me")
+async def me(current_customer = Depends(get_current_customer)):
+    return current_customer
--- backend/app/router/org.py
+++ backend/app/router/org.py
@@ -5,7 +5,7 @@ from database import get_async_db
 from models import org as models_org
 from crud import org
 
-from lib.auth import organization_login, get_current_organization
+from lib.auth import organization_login, get_current_organization, organization_logout
 
 router = APIRouter()
 
@@ -16,7 +16,8 @@ async def add_org(add_org_request: models_org.AddOrgRequest, db = Depends(get_as
     name = add_org_request.name
     email = add_org_request.email
     password = add_org_request.password
-    return await org.add_org(db, name, email, password)
+    phone = add_org_request.phone
+    return await org.add_org(db=db, name=name, email=email, phone=phone, password=password)
 
 
 @router.post("/login")
@@ -30,7 +31,15 @@ async def add_store(add_store_request: models_org.AddStoreRequest, db = Depends(
     email = add_store_request.email
     password = add_store_request.password
     address = add_store_request.address
+    phone = add_store_request.phone
+    latitude = add_store_request.latitude
+    longitude = add_store_request.longitude
     open_time = add_store_request.open_time
     close_time = add_store_request.close_time
     organization_id = current_org.organization_id
-    return await org.add_store(db, organization_id, name, email, password, address, open_time, close_time)
+    return await org.add_store(db=db, organization_id=organization_id, name=name, email=email, password=password, address=address, phone=phone, latitude=latitude, longitude=longitude, open_time=open_time, close_time=close_time)
+
+
+@router.post("/logout")
+async def logout_org(_ = Depends(organization_logout)):
+    return
