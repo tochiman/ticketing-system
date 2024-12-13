777e9f5d146559ec522a0e05398053b22c5777b0
kknmt <m20109@g.metro-cit.ac.jp>
2024-12-14 00:51:49 +0900

    Add オーダーの一覧を取得するAPI を作成した

--- backend/app/crud/store.py
+++ backend/app/crud/store.py
@@ -1,1 +1,17 @@
+from models import Order, OrderDetail, Item, Store
+from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload
 from crud import models
+
+async def get_order_list(db, store_id):
+    query = (
+        select(Order)
+        .join(OrderDetail)
+        .join(Item)
+        .join(Store)
+        .where(Store.store_id == store_id)
+        .options(selectinload(Order.order_details).joinedload(OrderDetail.item))
+    )
+    result = await db.execute(query)
+    db_obj = result.scalars().all()
+    return db_obj
--- backend/app/models/store.py
+++ backend/app/models/store.py
@@ -1,1 +1,25 @@
-from models.base import BaseSchema
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List
+
+class OrderDetailResponse(BaseModel):
+    order_detail_id: str
+    item_id: str
+    number_of_purchase: int
+
+    class Config:
+        orm_mode = True
+
+class OrderResponse(BaseModel):
+    order_id: str
+    status: int
+    order_details: List[OrderDetailResponse]
+
+    class Config:
+        orm_mode = True
+
+class OrderListResponse(BaseModel):
+    orders: List[OrderResponse]
+
+    class Config:
+        orm_mode = True
